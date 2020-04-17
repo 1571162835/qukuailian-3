18cb6ba32cbd5b1d14bf4d1f2062c28fc4d1142b
xuwenrui <xuwenrui111@163.com>
2020-04-17 19:05:24 +0800

    [ADD] 新增产品的增删改查

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from app.views.user import user_page
+from app.views.product import product_page
 from app.extension import db
 import os
 
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql@localhost/app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "lalskskskskksksjsj"
 
 db.init_app(app)
 
 app.register_blueprint(user_page)
-
+app.register_blueprint(product_page)
 
---
+++ app/views/product.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+from ..extension import db
+from ..models import Product
+
+
+product_page = Blueprint('product_page', __name__)
+
+
+@product_page.route('/products/list')
+def product_list():
+    products = Product.query.all()
+    return render_template('products/list.html', products=products)
+
+
+@product_page.route('/products/add', methods=['GET', 'POST'])
+def product_add():
+    if request.method == 'GET':
+        return render_template('products/addproduct.html')
+    else:
+        name = request.form.get('name')
+        number = request.form.get('number')
+        dest = request.form.get('dest')
+        desc = request.form.get('desc')
+        if name and number and dest:
+            product = Product(name, int(number), dest, 1, desc)
+            db.session.add(product)
+            db.session.commit()
+            return '1'
+        else:
+            return '0'
+
+
+@product_page.route('/products/delete/<int:product_id>')
+def product_delete(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for('product_page.product_list'))
+
+
+@product_page.route('/products/modify/<int:product_id>', methods=['GET', 'POST'])
+def product_modify(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    if request.method == 'GET':
+        return render_template('products/modifyproduct.html', product=product)
+    else:
+        name = request.form.get('name')
+        number = request.form.get('number')
+        dest = request.form.get('dest')
+        desc = request.form.get('desc')
+        if name and number and dest:
+            product.name, product.number, product.dest, product.desc = name, number, dest, desc
+            db.session.commit()
+            return '1'
+        else:
+            return '0'
