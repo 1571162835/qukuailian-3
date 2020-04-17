7e13b1540cc6e39245983ed167679d25d63541d4
RainLISIYU <45550242+RainLISIYU@users.noreply.github.com>
2020-04-17 09:21:03 +0800

    update __init__.py
    修改sql连接

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from .views.user import user_page
 from .extension import db
 
 app = Flask('first_flask')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:lsy19980104@127.0.0.1/pydb'
+app.config['SQLALCHEMY_DATABASE_URI'] = '数据库连接'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "lalskskskskksksjsj"
 
